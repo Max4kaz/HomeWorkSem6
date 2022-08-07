dddf357368c346989245521fbbc831c1cfcc09b1
Maxim <max4kaz@gmail.com>
2022-08-07 20:14:36 +0700

    Add all Tasks HomeWorkSem6

---
+++ Task041_CountPosNums/Program.cs
@@ -0,0 +1,59 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите последовательность чисел через запятую: ");
+string sequenceNumbers = Console.ReadLine();
+
+int[] arrayNumbers = CheckArray(sequenceNumbers, ',');
+PrintArray(arrayNumbers);
+
+int countPosNums = 0;
+for (int i = 0; i < arrayNumbers.Length; i++)
+{
+    if (arrayNumbers[i] > 0)
+        countPosNums++;
+}
+Console.WriteLine("Количество чисел больше нуля = " + countPosNums);
+
+int[] CheckArray(string input, char comma)
+{
+    int[] numbers = new int[countCommas(input)];
+    string numberWithoutComma = String.Empty;
+    int numberIndex = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == comma)
+        {
+            numbers[numberIndex] = Convert.ToInt32(numberWithoutComma);
+            numberIndex++;
+            numberWithoutComma = String.Empty;
+        }
+        else
+        {
+            numberWithoutComma += input[i];
+        }
+    }
+    numbers[numberIndex] = Convert.ToInt32(numberWithoutComma);
+    return numbers;
+}
+
+int countCommas(string numbers)
+{
+    int count = 1;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] == ',')
+            count++;
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ Task043_CrossPoint/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.WriteLine("Для нахождения точки пересечения двух прямых введите переменные:");
+double k1 = InputInt("k1 = ");
+double b1 = InputInt("b1 = ");
+double k2 = InputInt("k2 = ");
+double b2 = InputInt("b2 = ");
+
+if (k1 == k2)
+{
+    Console.WriteLine("Точки пересечения нет, т.к. прямые параллельны");
+    return;
+}
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения двух прямых равна [{x}, {y}]");
+
+double InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
